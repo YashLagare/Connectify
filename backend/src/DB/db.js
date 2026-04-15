7b255bc48812451c236f09fc08e6058037dcc91e
YashLagare <yashlagare77@gmail.com>
2026-04-15 11:23:32 +0530

    Refactor connectDB function formatting for improved readability

--- backend/src/DB/db.js
+++ backend/src/DB/db.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 import { ENV } from "../config/env.js";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(ENV.MONGO_URI);
-        console.log("MongoDB connected:", conn.connection.host);
-    } catch (error) {
-        console.error("MongoDB connection failed:", error.message);
-        process.exit(1);
-    }
-}
-
+  try {
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MongoDB connected:", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
