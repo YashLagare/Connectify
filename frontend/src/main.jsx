5c489ab1a6187153cd39c17bffa78d81d970d112
YashLagare <yashlagare77@gmail.com>
2026-04-26 07:42:45 +0530

    updated-code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import { ClerkProvider } from '@clerk/clerk-react'
+import * as Sentry from "@sentry/react"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
-import { BrowserRouter } from 'react-router'
+import {
+  BrowserRouter,
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType
+} from "react-router"
 import App from './App.jsx'
 import './index.css'
 import AuthProvider from './providers/AuthProvider.jsx'
@@ -16,6 +23,21 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 
 
 createRoot(document.getElementById('root')).render(
