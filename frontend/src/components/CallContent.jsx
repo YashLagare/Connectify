b2b1b3cf452cceb717497fe406bea341f117dcd6
YashLagare <yashlagare77@gmail.com>
2026-05-13 07:31:46 +0530

    call-page issue-solved

--- frontend/src/components/CallContent.jsx
+++ frontend/src/components/CallContent.jsx
@@ -1,22 +1,32 @@
-import { CallControls, SpeakerLayout, StreamTheme } from '@stream-io/video-react-sdk';
+import {
+    CallControls,
+    CallingState,
+    SpeakerLayout,
+    StreamTheme,
+    useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const CallContent = () => {
-    const CallContent = () => {
-        const { useCallCallingState } = useCallStateHooks();
+    const { useCallCallingState } = useCallStateHooks();
 
-        const callingState = useCallCallingState();
-        const navigate = useNavigate();
+    const callingState = useCallCallingState();
+    const navigate = useNavigate();
 
-        if (callingState === CallingState.LEFT) return navigate("/");
+    useEffect(() => {
+        if (callingState === CallingState.LEFT) {
+            navigate("/");
+        }
+    }, [callingState, navigate]);
 
-
-    };
     return (
         <StreamTheme>
             <SpeakerLayout />
             <CallControls />
         </StreamTheme>
     );
-}
+};
 
-export default CallContent
+export default CallContent;
